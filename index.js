cac2dbb41dd55b027dec354ea047fcd82f4714dd
Zachary Mowatt <zamowatt@gmail.com>
2023-12-21 00:40:14 -0500

    Start new recipe entry section

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function loadRecipes(){
 
 //Render each recipe
 function renderRecipe(recipe){
-    let cookBook = document.querySelector("#recipe-list");
-    let newRecipe = document.createElement('li');
+    const cookBook = document.querySelector("#recipe-list");
+    const newRecipe = document.createElement('li');
 
     newRecipe.className = "meal";
     newRecipe.innerHTML = `
@@ -20,15 +20,41 @@ function renderRecipe(recipe){
         <button class="delete-button">Delete Recipe</button>
     </div>
     `
-    cookBook.appendChild(newRecipe);
+    cookBook.appendChild(newRecipe);   
+} 
 
-    let addButton = newRecipe.querySelector('.add-button');
-    addButton.addEventListener('click', addIngredients);
+//Add more ingredient entry options
+const ingredientsEntry = document.querySelector('#ingredients-entry');
+const moreIngredients = document.querySelector('#more-ingredients');
 
-    let deleteButton = newRecipe.querySelector('.delete-button');
-    deleteButton = newRecipe.addEventListener('click', deleteMeal)
-}
+moreIngredients.addEventListener('click', (e) => {
+    e.preventDefault();
+    const newEntry = document.createElement('input');
+    const newLine = document.createElement('br');
+    
+    newEntry.type = "text"
+    newEntry.placeholder = "Ingredient"
 
+    ingredientsEntry.appendChild(newEntry)
+    ingredientsEntry.appendChild(newLine)
+
+})
+
+// let addButton = newRecipe.querySelector('.add-button');
+// addButton.addEventListener('click', addIngredients);
+
+// let deleteButton = newRecipe.querySelector('.delete-button');
+// deleteButton = newRecipe.addEventListener('click', deleteMeal)
+
+// function addIngredients(recipe){
+//     let ingredient = document.createElement('li');
+//     ingredient.innerText  = `${recipe.ingredients}`
+
+//     let groceryList = document.querySelector('#grocery-list')
+
+//     groceryList.appendChild(ingredient);
+
+// }
 
 //Initialize
 loadRecipes()
