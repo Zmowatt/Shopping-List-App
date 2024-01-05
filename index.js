0cb7709dddd72083ac35d372da7348c4bd26862f
Zachary Mowatt <zamowatt@gmail.com>
2024-01-04 21:55:00 -0500

    Restructured page, and can now use the add-item bar, almost

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function renderRecipe(recipe){
 
     newRecipe.className = "meal";
     newRecipe.innerHTML = `
-    <div>
+    <div style="display: flex">
         <p>${recipe.meal}</p>
         <button class="add-button">Add to Shopping List</button>
         <button class="delete-button">Delete Recipe</button>
@@ -49,6 +49,7 @@ function loadShopList(){
         })
 }
 
+//Render Each Shopping List Item from Server
 function renderShopList(buyMe){
     const groceryList = document.querySelector("#grocery-list");
     const newItem = document.createElement('li');
@@ -56,28 +57,40 @@ function renderShopList(buyMe){
     newItem.className = "item";
     newItem.innerHTML = `
     <div style="display: flex">
-        <p style="margin-right: 10px">${buyMe.item}</p>
-        <button class="delete-button" style="font-size: 14px; padding-top: -4px">Delete</button>
+        <p style="margin-right: 10px; font-size: 14px">${buyMe.item}</p>
+        <button class="delete-button" style="font-size: 12px; padding: 6px 10px">Delete</button>
     </div>
     `
+
+    let deleteButton = newItem.querySelector('.delete-button');
+    deleteButton.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'red';
+    })
+    deleteButton.addEventListener('mouseout', (event) => {
+        event.target.style.backgroundColor = '';
+    })
+    deleteButton.addEventListener('click', deleteItem)
     groceryList.appendChild(newItem);   
 }
 
+function deleteItem(item){
+    fetch('http://localhost:3000/shopping-list')
 
+}
 //------------------------------------------------------------------
 //Add Misc. Items to Shopping List
 addItemButton = document.querySelector('#submit-item')
 
-addItemButton.addEventListener('submit', (e) => {
+addItemButton.addEventListener('click', (e) => {
     e.preventDefault();
     const groceryList = document.querySelector('#grocery-list')
-    const newItemInput = document.querySelector('input[name="item"]');
+    const newItemInput = document.querySelector('#pickMeUp');
     newItemText = newItemInput.value;
 
-    // let newIngredient = document.createElement('li'),
-    // newIngredient.textContent = newItemText;
-    // groceryList.appendChild(newItem);
-    // newItemInput.value ='';  
+    let newIngredient = document.createElement('li');
+    newIngredient.textContent = newItemText;
+    groceryList.appendChild(newIngredient);
+    newItemInput.value ='';  
 });
 
 
